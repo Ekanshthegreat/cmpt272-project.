abdce0f06e0f396d323e77516e759de13713c71a
vancarii <yechengofficial@gmail.com>
2024-11-18 19:12:44 -0800

    added folders and files for diff roles

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Login() {
+  return (
+    <div>
+      This is login page
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <ModeToggle />
+      <Link href="/login">
+        <Button>Login</Button>
+      </Link>
     </div>
   );
 }
